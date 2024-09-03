9cbf7ff71b560ba7c3490fdfd9e2083e3b09b3c2
Andrew <andrew18523@gmail.com>
2024-09-03 11:51:28 +0800

    update

---
+++ BOOTCAMP_EXERCISE1.sql
@@ -0,0 +1,159 @@
+create database BOOTCAMP_EXERCISE1;
+
+use BOOTCAMP_EXERCISE1;
+
+create table regions(
+	region_id int primary key,
+    region_name varchar(25)
+);
+
+create table countries (
+	country_id varchar(2) Primary Key,
+    country_name varchar(40),
+    region_id int,
+    foreign key (region_id) references regions(region_id)
+);
+
+create table locations (
+	location_id int Primary Key,
+    street_address varchar(25),
+    postal_code varchar(12),
+    city varchar(30),
+    state_province varchar(12),
+    country_id varchar(2),
+    foreign key (country_id) references countries(country_id)
+);
+
+create table jobs(
+	job_id varchar(10) Primary Key,
+    job_title varchar(35),
+    min_salary int,
+    max_salary int
+);
+
+create table departments(
+	department_id int primary key,
+    department_name varchar(30),
+    manager_id int,
+    location_id int,
+    foreign key (location_id) references locations(location_id)
+);
+
+create table job_history(
+	employee_id int ,
+    start_date date,
+    end_date date,
+    job_id varchar(10),
+    department_id int,
+    primary key(employee_id,start_date),
+    foreign key (job_id) references jobs(job_id),
+    foreign key (department_id) references departments(department_id)
+);
+
+create table employees (
+	employee_id int primary key,
+    first_name varchar(20),
+    last_name varchar(25),
+    email varchar(25),
+    phone_number varchar(20),
+    hire_date date,
+    job_id varchar(10),
+    salary int,
+    commission_pct int,
+    manager_id int,
+    department_id int,
+    foreign key(employee_id) references job_history(employee_id),
+    foreign key(job_id) references jobs (job_id),
+    foreign key (department_id) references departments(department_id)
+);
+
+-- 2
+
+insert into regions (region_id , region_name) values(1,'Asia');
+insert into regions (region_id , region_name) values(2,'Europe');
+
+insert into countries values(1, 'Hong Kong',1);
+insert into countries values(2, 'United Kingdom',2);
+
+insert into locations values (1, 'No.1, NT Road', '000000', 'Prince Edward', 'Kowloon','1');
+insert into locations values (2, 'No.1, London Road', '312456', 'London', 'London','2');
+
+insert into departments values (1, 'Marting', 1, 1);
+insert into departments values (2, 'IT', 14, 1);
+insert into departments values (3, 'HR', 33, 2);
+insert into departments values (4, 'Admin', 66, 2);
+
+insert into jobs values ('IT_PRO', 'IT Programming', 10000,20000);
+insert into jobs values ('MK_PRO', 'Marketing Promote', 8000,12000);
+insert into jobs values ('ST_CLERK', 'Standard Clerk', 6000,10000);
+insert into jobs values ('SAL_ADJ', 'Salary Adjustment', 13000,16000);
+insert into jobs values ('Bou_cal', 'Bouns Calculation', 3000,4000);
+
+insert into job_history values (101,date_format('2000-01-13','%Y-%m-%d'),date_format('2001-07-24','%Y-%m-%d'),'IT_PRO',2);
+insert into job_history values (102,date_format('2001-09-21','%Y-%m-%d'),date_format('2002-10-27','%Y-%m-%d'),'MK_PRO',1);
+insert into job_history values (103,date_format('2002-10-28','%Y-%m-%d'),date_format('2003-03-15','%Y-%m-%d'),'MK_PRO',1);
+insert into job_history values (104,date_format('2006-02-17','%Y-%m-%d'),date_format('2007-12-19','%Y-%m-%d'),'ST_CLERK',4);
+insert into job_history values (102,date_format('2002-03-24','%Y-%m-%d'),date_format('2003-12-31','%Y-%m-%d'),'MK_PRO',1);
+insert into job_history values (105,date_format('2002-05-23','%Y-%m-%d'),date_format('2003-12-31','%Y-%m-%d'),'Bou_cal',3);
+
+insert into employees values (101,'Peter','Chan','peter.chan@gmail.com','9871234',date_format('1991-01-04','%Y-%m-%d'),'IT_PRO',20000,0,null,2);
+insert into employees values (102,'Carman','Wong','carman.wongn@gmail.com','95612367',date_format('1993-02-04','%Y-%m-%d'),'MK_PRO',15000,15,null,1);
+insert into employees values (103,'John','Lam','john.lam@gmail.com','90845938',date_format('1985-04-21','%Y-%m-%d'),'MK_PRO',40000,0,1,1);
+insert into employees values (104,'Ming','Sze','mong.sze@gmail.com','65478321',date_format('1987-12-23','%Y-%m-%d'),'ST_CLERK',16000,0,null,4);
+insert into employees values (105,'Vivian','Chan','vivian.chan@gmail.com','68970345',date_format('1993-04-04','%Y-%m-%d'),'Bou_cal',23000,0,null,3);
+
+-- 3
+
+select l.location_id, l.street_address, l.state_province, c.country_name
+from locations l left join countries c on l.country_id = c.country_id;
+
+-- 4
+
+select e.first_name , e.last_name, e.department_id
+from employees e;
+
+-- 5 in London
+select e.first_name , e.last_name, e.job_id, e.department_id
+from employees e left join (
+select d.department_id
+from departments d left join locations l on d.location_id = l.country_id
+where l.country_id = 2
+) x on x.department_id = e.department_id
+where x.department_id is not null;
+
+-- 6
+select e.first_name , e.last_name, e.manager_id
+from employees e
+where e.manager_id is not null;
+
+-- 7 after Peter Chan
+select e.first_name , e.last_name, e.hire_date
+from employees e where exists ( select 1 from (select min(e.hire_date) as min
+from employees e 
+where e.hire_date > '1991-01-04'
+order by e.hire_date) x where e.hire_date = x.min);
+
+-- 8
+
+select d.department_name, count(d.department_name) as employees_count
+from departments d left join employees e on d.department_id = e.department_id
+group by d.department_name;
+
+-- 9 (department_id = 1, Marketing)
+
+select jh.employee_id,j.job_title, datediff(jh.end_date,jh.start_date)
+from job_history jh left join jobs j on jh.job_id = j.job_id
+where jh.department_id = 1;
+
+-- 10 
+
+select d.department_name, d.manager_id, x.city, x.country_name
+from departments d left join
+( select l.location_id, l.city, c.country_name
+from locations l left join countries c on c.country_id = l.country_id
+) x on x.location_id = d.location_id;
+
+-- 11
+select d.department_name, avg(e.salary) as average_salary
+from employees e left join departments d on d.department_id = e.department_id
+group by d.department_name;
--- Day 1.sql
+++ Day 1.sql
@@ -1,6 +1,6 @@
 -- comment
 -- database -> table (row x column)
--- create database db_bc2405p; 
+create database db_bc2405p; 
 
 use db_bc2405p;
 
@@ -39,18 +39,320 @@ create table students(
 
 insert into students (id, name, weight, height) values (1, 'John Lau', 85.32, 170.21);
 insert into students (id, name, weight, height) values (2, 'Peter Chan', 100.21, 183.1);
-insert into students (id, name, weight, height) values (3.5, 'Peter Wong', 90, 12);
+insert into students (id, name, weight, height) values (3.5, 'Peter Wong', 90, 140);
 
-insert into students (id, name, weight, height) values (5, 'Peter Chan', 90.999, 12);
+insert into students (id, name, weight, height) values (5, 'Peter Man', 90.999, 170);
+-- You can ship some column(s) when you preform insert statement
+insert into students (id, name, weight) values (6, 'Simon Wong', 93.124);
 
+-- If you don't specifiy the column name, then you have to put all column values
+insert into students  values (7, 'Simon Chan', 93.124,null);
 select * from students;
 
+-- DDL (Data Definition Language): create/drop table, add/drop column, modify (modify column defintion varchar(50)-> varchar(100)) 
+-- DML (Data Manipulation Language): insert row, update column, delete row, trumcate table (remove all data)
+
+-- +,-,*,/,%
+select weight + height as ABC , weight, height , name from students;
+
+select s.weight + s.height as DEF, s.* from students s where s.weight >90 order by name;
+
+-- >,<,>=, <=, <>/!=, = 
+select * from students where id<>6 and id<>8;
+
+-- not in
+select * from students where id not in (6,7);
+select * from students where name = 'John Lau';
+
+-- Any name with prefix (0 or more character) and suffix (0 or more character)
+select * from students where name like '%Wong%';
+
+select * from students where name not like '%Wong%';
+
+-- null check
+select * from students where weight is null or height is null;
+select * from students where weight is not null and height is not null;
+
+-- Functions
+insert into students (id,name,weight, height) values(9, '甲乙',60.35,180.5);
+
+-- substring() - start from 1
+select upper(s.name) ,lower(s.name), substring(s.name ,1,3),trim(s.name),replace(s.name, 'Wong', 'Chan'), s.* from students s;
+
+-- Java: indexOf(), DB instr() position start from 1, 0 = not found
+select instr(s.name, 'Wong'), s.* from students s;
+
+create table orders (
+	id integer,
+    total_amount numeric (10,2),
+    customer_id integer
+    
+);
+
+select * from orders; -- List<Object>
+insert into orders values (1,205.10,2);
+insert into orders values (2,10000.9,2);
+insert into orders values (3,99.9,1);
+insert into orders values (4,1999.9,3);
+insert into orders values (5,999.99,4);
+insert into orders values (6,999.99,5);
+
+-- sum()
+-- 3ms (without network consideration)
+select sum(o.total_amount) from orders o;
+select avg(o.total_amount) from orders o where customer_id = 2 ;
+select min(o.total_amount), max(o.total_amount) from orders o where customer_id = 2;
+select min(o.total_amount), max(o.total_amount) from orders o;
+
+select o.*,1 as number,'hello' as string from orders o;
+
+-- why can we put all fucntions is select statement?
+-- Ans: Aggregation Functions
+select sum(o.total_amount), avg(o.total_amount),min(o.total_amount), max(o.total_amount), count(o.total_amount) from orders o;
+
+-- select o.total_amount, sum(o.total_amount) from orders o; -- error
+
+-- group by -> select "group key"
+select * from orders;
+select customer_id , sum(total_amount) from orders o group by o.customer_id;
+select customer_id , sum(total_amount), group_concat(id SEPARATOR ';') from orders o group by o.customer_id;
+
+-- filter befroe groupby
+select customer_id ,sum(total_amount) 
+from orders o 
+where o.customer_id in (1,2,3,5) 
+group by o.customer_id;
+-- 2	10206.00
+-- 1	99.90
+-- 3	1999.90
+-- 5	999.99
+
+-- filter after group by
+select customer_id , sum(total_amount) from orders o group by o.customer_id having sum(total_amount) >100;
+-- 2	10206.00
+-- 3	1999.90
+-- 4	999.99
+-- 5	999.99
+
+select customer_id ,sum(total_amount) 
+from orders o 
+where o.customer_id in (1,2,3,5) 
+-- 2	10206.00
+-- 1	99.90
+-- 3	1999.90
+-- 5	999.99
+group by o.customer_id
+having sum(total_amount) >1000;
+-- 2	10206.00
+-- 3	1999.90
+
+select * from customers where name = 'JOHN LAU';
+-- select * from customers where name collate utf8_bin = 'JOHN LAU';
+
+select * from customers where name like  '_____LAU';
+select * from customers where name like  '%LAU';
+select * from customers where name like  '____LAU';
+
+select Round(total_amount, 0), o.* from orders o;
+
+select date_format('2023-08-31','%Y-%m-%d') from dual;
+select date_format('2023-08-31','%Y-%m-%d')+1 from dual; -- 24
+select date_format('2023-08-31','%Y-%m-%d') + interval 1 day from dual;
+select str_to_date('2023-08-31','%Y-%m-%d') + interval 1 day from dual;
+
+select extract(Year From Date_format('2023-08-31','%Y-%m-%d')) from dual;
+select extract(month From Date_format('2023-08-31','%Y-%m-%d')) from dual;
+select extract(day From Date_format('2023-08-31','%Y-%m-%d')) from dual;
+
+select * from orders;
+alter table orders add column trans_date date;
+
+select extract(Year from trans_date) as YEAR , count(id) as NUMBER_OF_ORDERS
+from orders
+group by extract(Year from trans_date)
+having NUMBER_OF_ORDERS>=2;
 
 
+update orders
+set trans_date = date_format('2023-08-31','%Y-%m-%d')
+where id = 1;
 
+update orders
+set trans_date = date_format('2022-08-30','%Y-%m-%d')
+where id = 2;
 
+update orders
+set trans_date = date_format('2023-07-29','%Y-%m-%d')
+where id = 3;
 
+update orders
+set trans_date = date_format('2023-08-28','%Y-%m-%d')
+where id = 4;
+update orders
+set trans_date = date_format('2024-06-27','%Y-%m-%d')
+where id = 5;
+update orders
+set trans_date = date_format('2023-08-26','%Y-%m-%d')
+where id = 6;
 
+select * from orders;
 
+-- coalesce did not update the original data
+select ifnull(s.weight,'N/A'),ifnull(s.height,'N/A'), s.* from students s;
+select ifnull(s.weight,'N/A'),coalesce(s.height,'N/A'), s.* from students s;
 
+-- <1000 'S'
+-- >= 1000 and <= 10000 'M'
+-- >=10000 'L'
+select case
+		when total_amount <1000 Then 'S'
+        when total_amount >= 1000 and total_amount <10000 then 'M'
+        else 'L'
+	END as category
+    ,o.*
+from orders o;
 
+select * from orders;
+
+select *
+from orders
+where trans_date between date_format('2023-01-31', '%Y-%m-%d')
+and date_format('2023-08-30', '%Y-%m-%d');
+
+select * from customers;
+
+insert into customers values (3,'Jenny Wong', 'jenny@gmail.com');
+insert into customers values (4,'Ling Wong', 'ling@gmail.com');
+insert into customers values (5,'Kalvin Chan', 'Kalvin@gmail.com');
+insert into customers values (6,'Sue lee', 'sue@gmail.com');
+
+--  where o.customer_id=c.id -> check if the customer exist in orders
+-- Approach 1
+select *
+from customers c
+where exists (select 1 from orders o where o.customer_id=c.id);
+
+-- Approach 2
+-- JOIN tables
+-- 6 customers * 6 orders = 36 rows
+select *
+from customers c inner join orders o;
+
+select c.*, o.*
+from customers c inner join orders o on (o.customer_id = c.id);
+
+select *
+from customers c
+where not exists (select 1 from orders o where o.customer_id=c.id);
+
+-- '2024-08'
+-- Distinst 1 columns
+select distinct Concat_ws('-',extract(Year from trans_date) , extract(Month from trans_date)) from orders;
+
+-- Distinst 2 columns
+select distinct Concat_ws('-',extract(Year from trans_date) , extract(Month from trans_date)), total_amount from orders;
+
+select o.* ,(select max(total_amount) from orders)
+from orders o;
+
+-- Subquery
+-- First SQL to excute select id from customers where name like '%LAU'
+-- Second, DBMS excute select * from orders where customer_id in
+select *
+from orders
+where customer_id in (select id from customers where name like '%LAU'); 
+
+select * from orders;
+
+insert into orders values (7, 400.0, null,date_format('2023-09-01','%Y-%m-%d'));
+
+-- LEFT JOIN
+select c.*,o.*
+from customers c left join orders o on c.id=o.customer_id;
+
+select o.*, c.*
+from orders o left join customers c on c.id=o.customer_id;
+
+-- right join
+select  o.* ,c.*
+from customers c right join orders o on c.id=o.customer_id;
+
+select c.*, o.*
+from orders o right join customers c on c.id=o.customer_id;
+
+-- left join + group by
+-- count(o.id) is different to count(c.id)
+-- Step 1: Left Join (key)
+-- Step 2: Where
+-- Step 3: group by
+-- Step 4: order by
+-- Step 5: select
+select c.id,c.name,count(o.id) as number_of_orders, ifnull(max(o.total_amount),0) as max_amount_of_orders
+from customers c left join orders o on c.id = o.customer_id
+where o.total_amount >100.0
+group by c.id,c.name
+order by c.name asc; 
+
+select * from customers;
+insert into customers value (4,'Mary Chan', 'mary@gmail.com');
+delete from customers where name ='Mary Chan';
+
+-- DDL
+-- Add PK
+alter table customers add constraint pk_customer_id primary key(id);
+-- insert into customers value (4,'Mary Chan', 'mary@gmail.com'); -- error
+insert into customers value (7,'Mary Chan', 'mary@gmail.com'); -- ok
+
+-- Add FK
+alter table orders add constraint fk_customer_id Foreign key(customer_id) references customers(id);
+
+select * from orders;
+-- insert into orders values (8, 400.0, 10,date_format('2023-09-01','%Y-%m-%d')); -- error as orders. customer_id (10) is not in customers.id
+insert into orders values (8, 400.0, 5,date_format('2023-09-01','%Y-%m-%d'));
+
+-- Table design: PK and FK to ensure Data is insert or update with integrity, consistancy
+-- Primary Key and Forign Ket are also a type of constriants
+
+-- Other Constraint: Unique Constraint 
+select * from customers;
+
+alter table customers add constraint unique_email unique (email);
+-- insert into customers value (8,'Mary Chan', 'mary@gmail.com'); -- error
+
+-- not null
+alter table customers modify name varchar(50) not null;
+-- insert into customers value (9,null, 'mary@gmail.com'); -- error as name is null
+
+create table Department (
+	department_id int Primary Key,
+    department_name VARCHAR(50)
+);
+
+create table Empolyee (
+	employee_id INT Primary key,
+    first_name VARCHAR(50) not null,
+    last_name VARCHAR(50) not null,
+    department_id int,
+    Foreign Key (department_id) references department(department_id)
+)
+
+select name, email from customers;
+
+select name, email
+from customers
+union all
+select id, total_amount
+from orders;
+
+select 1
+from customers
+union all
+select 1
+from orders;
+
+-- Distinst, combine two result set no matter any duplicated -  just one '1'
+select 1
+from customers
+union
+select 1
+from orders;
